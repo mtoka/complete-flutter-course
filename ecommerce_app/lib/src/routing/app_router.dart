347bf6ec6af43995cea3950dc90dcdebc9e3f074
Marcin Tokarzewski <marcin_tokarzewski@o2.pl>
2024-09-12 15:01:14 +0200

    impolementing gorouter to remaining code

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,5 +1,9 @@
+import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -18,6 +22,35 @@ final goRouter = GoRouter(
             key: state.pageKey,
             fullscreenDialog: true,
             child: const ShoppingCartScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'orders',
+          // builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          // builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          // builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
+            ),
           ),
         )
       ],
