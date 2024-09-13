9dff7a04ff58f01647f1200d78facc1129670750
Marcin Tokarzewski <marcin_tokarzewski@o2.pl>
2024-09-13 09:31:31 +0200

    implemented enums into gorouter

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -7,16 +7,20 @@ import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+enum AppRoute { home, cart, orders, account, signIn }
+
 final goRouter = GoRouter(
   initialLocation: '/',
   debugLogDiagnostics: true,
   routes: [
     GoRoute(
       path: '/',
+      name: AppRoute.home.name,
       builder: (context, state) => const ProductsListScreen(),
       routes: [
         GoRoute(
           path: 'cart',
+          name: AppRoute.cart.name,
           // builder: (context, state) => const ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
@@ -26,6 +30,7 @@ final goRouter = GoRouter(
         ),
         GoRoute(
           path: 'orders',
+          name: AppRoute.orders.name,
           // builder: (context, state) => const ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
@@ -35,6 +40,7 @@ final goRouter = GoRouter(
         ),
         GoRoute(
           path: 'account',
+          name: AppRoute.account.name,
           // builder: (context, state) => const ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
@@ -44,6 +50,7 @@ final goRouter = GoRouter(
         ),
         GoRoute(
           path: 'signIn',
+          name: AppRoute.signIn.name,
           // builder: (context, state) => const ShoppingCartScreen(),
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
