956035e9e8eff1ffa63dd635dbe172de9714d760
Marcin Tokarzewski <marcin_tokarzewski@o2.pl>
2024-09-13 09:32:24 +0200

    updated all routes with named routes with enums

--- ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
+++ ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
@@ -1,8 +1,5 @@
-import 'package:ecommerce_app/src/features/account/account_screen.dart';
-import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/models/app_user.dart';
 import 'package:go_router/go_router.dart';
@@ -54,33 +51,13 @@ class MoreMenuButton extends StatelessWidget {
         // push to different routes based on selected option
         switch (option) {
           case PopupMenuOption.signIn:
-            context.go('/singIn');
-            // Navigator.of(context).push(
-            //   MaterialPageRoute(
-            //     fullscreenDialog: true,
-            //     builder: (_) => const EmailPasswordSignInScreen(
-            //       formType: EmailPasswordSignInFormType.signIn,
-            //     ),
-            //   ),
-            // );
+            context.goNamed(AppRoute.signIn.name);
             break;
           case PopupMenuOption.orders:
-            context.go('/orders');
-            // Navigator.of(context).push(
-            //   MaterialPageRoute(
-            //     fullscreenDialog: true,
-            //     builder: (_) => const OrdersListScreen(),
-            //   ),
-            // );
+            context.goNamed(AppRoute.orders.name);
             break;
           case PopupMenuOption.account:
-            context.go('/account');
-            // Navigator.of(context).push(
-            //   MaterialPageRoute(
-            //     fullscreenDialog: true,
-            //     builder: (_) => const AccountScreen(),
-            //   ),
-            // );
+            context.goNamed(AppRoute.account.name);
             break;
         }
       },
